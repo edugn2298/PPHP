f1c17c089019e1e56f67030f7e75f33a24269bd5
Eduardo Garcia <84125798+edugn2298@users.noreply.github.com>
2024-10-25 00:13:27 -0400

    act

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  content: ["./view/**/*.{php,js}"],
+  content: ["./View/**/*.{php,js}"],
   theme: {
-    extend: {},
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#fd3d57",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
